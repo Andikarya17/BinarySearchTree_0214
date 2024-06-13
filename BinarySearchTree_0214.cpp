aa37ff2f80d15f6b4edeebba2b60c1b2260dbf60
Andikarya17 <andikaryaputra17@gmail.com>
2024-06-13 15:34:13 +0700

    membuat void search

--- BinarySearchTree_0214.cpp
+++ BinarySearchTree_0214.cpp
@@ -55,4 +55,6 @@ public:
             parent->rightchild = newNode; // make the right child of the parent point to the new node
         }
     }
+
+    void search(string element, node*& parent, node*& currentNode)
 }
